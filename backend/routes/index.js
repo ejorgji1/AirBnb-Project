470277327d7fd9d194201629a2c974d8122f3feb
Enea Jorgji <eneajorgji@Eneas-MacBook-Pro.local>
2023-06-02 15:43:06 -0400

    5th commit Auth phase 1

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
+const apiRouter = require('./api');
 // router.get('/hello/world', function(req, res) {
 //   res.cookie('XSRF-TOKEN', req.csrfToken());
 //   res.send('Hello World!');
@@ -13,5 +14,8 @@ router.get("/api/csrf/restore", (req, res) => {
     'XSRF-Token': csrfToken
   });
 });
+
+router.use('/api', apiRouter);
+
 
 module.exports = router;
