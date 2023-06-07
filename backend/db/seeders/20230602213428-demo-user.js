85d251ddb562f7def28efbcb5494de3a3ec301bf
Enea Jorgji <eneajorgji@Eneas-MacBook-Pro.local>
2023-06-06 20:57:01 -0400

    reseding before deployment

--- backend/db/seeders/20230602213428-demo-user.js
+++ backend/db/seeders/20230602213428-demo-user.js
@@ -11,16 +11,22 @@ module.exports = {
     options.tableName = 'Users';
     return queryInterface.bulkInsert(options, [
       {
+        firstName:'Name1',
+        lastName:'Last1',
         email: 'demo@user.io',
         username: 'Demo-lition',
         hashedPassword: bcrypt.hashSync('password')
       },
       {
+        firstName:'Name2',
+        lastName:'Last2',
         email: 'user1@user.io',
         username: 'FakeUser1',
         hashedPassword: bcrypt.hashSync('password2')
       },
       {
+        firstName:'Name3',
+        lastName:'Last3',
         email: 'user2@user.io',
         username: 'FakeUser2',
         hashedPassword: bcrypt.hashSync('password3')
