9db8414fee6408f9ab0ca6d862792ceedd8a0a0b
Enea Jorgji <eneajorgji@Eneas-MacBook-Pro.local>
2023-06-15 13:33:57 -0400

    delete review image

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -6,6 +6,7 @@ const spotsRouter = require('./spots.js')
 const reviewsRouter = require('./reviews.js')
 const bookingsRouter = require('./bookings.js')
 const spotImageRouter = require('./spot-image.js')
+const reviewImageRouter = require('./review-image.js')
 const { restoreUser } = require("../../utils/auth.js");
 
 // Connect restoreUser middleware to the API router
@@ -20,6 +21,7 @@ router.use('/spots', spotsRouter);
 router.use('/reviews', reviewsRouter);
 router.use('/bookings', bookingsRouter);
 router.use('/spot-images', spotImageRouter)
+router.use('/review-images', reviewImageRouter)
 
 router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
