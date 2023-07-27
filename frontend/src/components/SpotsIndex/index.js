9288685cb02a9588ebab4c9a85f22e57409cc475
Enea Jorgji <eneajorgji@Eneas-MacBook-Pro.local>
2023-07-27 18:29:19 -0400

    all spots component

---
+++ frontend/src/components/SpotsIndex/index.js
@@ -0,0 +1,47 @@
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { allSpotsThunk } from "../../store/spot";
+import "./SpotsIndex.css";
+
+export default function SpotsIndex () {
+    const dispatch = useDispatch();
+    const spots = useSelector((state) => state.spots.allSpots);
+    console.log('this is ', spots);
+
+    useEffect(() => {
+        dispatch(allSpotsThunk());
+    },[dispatch]);
+    
+    return (
+        <div className="spots-container">
+          {spots &&
+            spots.map((spot) => (
+              <Link to={`/spots/${spot.id}`} >
+                <div className="spot" title={spot.name}>
+                  <img src={spot.previewImage} alt="Spot Preview" />
+                  <div className="location-and-rating">
+                  <p>
+                    {spot.city}, {spot.state}
+                  </p>
+                  {spot.avgStarRating === "NaN" ? (
+                    <div className="reviews">
+                      <i className="fa-solid fa-star"></i>
+                      <div className="newListing">New</div>
+                    </div>
+                  ) : (
+                    <div className="reviews">
+                        <i className="fa-solid fa-star"></i>
+                        <div className="avgRating">{spot.avgStarRating}</div>
+                    </div>
+                  )}
+                  </div>
+                  <div className="price-container">
+                    <div className="price">${spot.price}</div> night</div>
+                </div>
+              </Link>
+            ))}
+        </div>
+      );
+    }
+
