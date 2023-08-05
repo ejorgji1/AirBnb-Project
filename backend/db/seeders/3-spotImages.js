bcb84edb8681c1d802cde2c3b08bd7588d0f0312
Enea Jorgji <eneajorgji@Eneas-MacBook-Pro.local>
2023-08-05 13:44:00 -0400

    update image links

--- backend/db/seeders/3-spotImages.js
+++ backend/db/seeders/3-spotImages.js
@@ -14,53 +14,53 @@ module.exports = {
       {
        
         spotId: 1,
-        url: 'https://a0.muscache.com/im/pictures/0afff651-cafd-4d94-b694-808dfaf1f03b.jpg?im_w=1200',
+        url: 'https://images.pexels.com/photos/3617500/pexels-photo-3617500.jpeg?auto=compress&cs=tinysrgb&w=600.jpeg',
         preview: true
       },
       {
        
         spotId: 1,
-        url: 'https://a0.muscache.com/im/pictures/49682fe5-544c-443f-a085-6746f1c88dbc.jpg?im_w=1440',
+        url: 'https://images.pexels.com/photos/1933239/pexels-photo-1933239.jpeg?auto=compress&cs=tinysrgb&w=600.jpeg',
         preview: false
       },
       {
        
         spotId: 1,
-        url: 'https://a0.muscache.com/im/pictures/59d7ad94-9b88-4a68-93f7-22212e0e5bb1.jpg?im_w=1440',
+        url: 'https://images.pexels.com/photos/2362002/pexels-photo-2362002.jpeg?auto=compress&cs=tinysrgb&w=600.jpeg',
         preview: false
       },
       {
        
         spotId: 2,
-        url: 'https://a0.muscache.com/im/pictures/8380325/d9c118ca_original.jpg?im_w=1200',
+        url: 'https://images.pexels.com/photos/313782/pexels-photo-313782.jpeg?auto=compress&cs=tinysrgb&w=600.jpeg',
         preview: true
       },
       {
        
         spotId: 2,
-        url: 'https://a0.muscache.com/im/pictures/8380417/6efdc993_original.jpg?im_w=1440',
+        url: 'https://images.pexels.com/photos/2246476/pexels-photo-2246476.jpeg?auto=compress&cs=tinysrgb&w=600.jpeg',
         preview: false
       },
       {
        
         spotId: 2,
-        url: 'https://a0.muscache.com/im/pictures/8380437/5f5863dd_original.jpg?im_w=1440',
+        url: 'https://images.pexels.com/photos/2366008/pexels-photo-2366008.jpeg?auto=compress&cs=tinysrgb&w=600.jpeg',
         preview: false
       },
       {
         
         spotId: 3,
-        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-715372110983087676/original/a33f982c-49b3-41e2-bfa7-5ed126bcd350.jpeg?im_w=1200',
+        url: 'https://images.pexels.com/photos/1388069/pexels-photo-1388069.jpeg?auto=compress&cs=tinysrgb&w=600.jpeg',
         preview: true
       },{
         
         spotId: 3,
-        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-715372110983087676/original/fe49bd58-9518-4c0a-990a-58ae23af82ab.jpeg?im_w=1440',
+        url: 'https://images.pexels.com/photos/3879071/pexels-photo-3879071.jpeg?auto=compress&cs=tinysrgb&w=600',
         preview: false
       },{
         
         spotId: 3,
-        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-715372110983087676/original/76e1edeb-703b-41b2-801c-0246f646afe1.jpeg?im_w=1440',
+        url: 'https://images.pexels.com/photos/443446/pexels-photo-443446.jpeg?auto=compress&cs=tinysrgb&w=600.jpeg',
         preview: false
       }
     ], {});
